87c519027c743bc241ad6b476d93ef0546b9c8b0
Chiran Samiru Serasinghe <it23401976@my.sliit.lk>
2026-09-23 03:02:02 +0530

    chore(api): register slot service

--- SolarMicrogrid.API/Program.cs
+++ SolarMicrogrid.API/Program.cs
@@ -79,6 +79,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<StationService>();
+builder.Services.AddScoped<SlotService>();
 
 builder.Services.AddOpenApi();
 
